3c1fd6e2b87677e45dc0b7598f0e3414aeae6c03
Ruben Quijada <rubenquijada3@gmail.com>
2022-10-26 19:28:00 -0400

    ahora si?

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-#xd
+ from __future__ import print_function
+from msilib.schema import tables
+ import unittest
+ class TestFoo(unittest.TestCase):
+     def test_foo(self):
+             self.assertTrue(True)
+     def fun_not_run(self):
+             print("no run")
 
-from ast import main
-
-
-main 
+ unittest.main()
