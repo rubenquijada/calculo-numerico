1796fa931e72eb57eba0f2ea7028a0d0058f55d7
rubenquijada <116230006+rubenquijada@users.noreply.github.com>
2022-11-05 16:00:19 -0400

    Update teoremas.py

--- teoremas.py
+++ teoremas.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import numpy as np 
+
 def Valor_intermedio(f,a,b):
 	return f(a)*f(b)<0
 
@@ -8,4 +10,7 @@ if __name__ == '__main__':
 	f= lambda x :x**2-4
 	a=0
 	b=5
-	print(Valor_intermedio(f,a,b))
+	x=np.arange(f,a,x)
+	pr
+	print(Valor_intermedio(f,a,x))
+	print(x)
